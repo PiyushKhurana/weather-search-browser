a9cd891b76626a077075cd50527a196aff238040
PiyushKhurana <khuranapiyush27@gmail.com>
2019-08-13 18:54:55 +0530

    api integration

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
+const forecast = require('./api/forecast');
+const geocode = require('./api/geocode');
 
 
 const app = express();
@@ -23,10 +25,33 @@ app.get('',(req,res)=>{
     })
 })
 
+
+
+app.get('/weather',(req,res)=>{
+
+    if(!req.query.address)
+    return res.send({
+        error:"You must provide a address"
+    });
+
+    geocode(req.query.address,(error,{latitude,longitude,location}={})=>{
+        if(error)
+        return res.send({error:error});
+        forecast(latitude,longitude,(error,data)=>{
+            if(error)
+            return res.send({error:error});
+            
+            res.send({
+                forecast:data,
+                location:location,
+                address:req.query.address
+            })
+        })
+    })
+})
 app.get('*',(req,res)=>{
     res.send("<h1>Dont mess with url !!!</h1> <h2>Error 404  Page Not Found</h2>")
 })
-
 app.listen(port,()=>{
     console.log(`Server is up and running at port ${port}`);
 });
